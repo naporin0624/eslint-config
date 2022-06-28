aefdd15a262d446a687d984b2ce3ae2817be93c8
naporin0624 <naporin0624@gmail.com>
2022-06-29 03:43:28 +0900

    :+1: update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:import/recommended",
     "plugin:import/typescript",
-    "prettier",
+    "plugin:prettier/recommended",
   ],
   root: true,
   rules: {
