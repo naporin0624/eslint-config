10f1723cde492ba9f97bd283eb6754b5ff1b4a06
naporin0624 <naporin0624@gmail.com>
2022-12-12 02:32:58 +0900

    ➕ add a11y plugin

--- react.js
+++ react.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["plugin:react/recommended", "plugin:react-hooks/recommended"],
+  extends: ["plugin:react/recommended", "plugin:react-hooks/recommended", "plugin:jsx-a11y/recommended"],
   rules: {
     "react/prop-types": "off",
     "react/display-name": "off",
